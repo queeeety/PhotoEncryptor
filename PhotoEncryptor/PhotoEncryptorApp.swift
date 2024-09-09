5f0d389234f430efa5650ea93769918de45df093
Tymofii Bezverkhyi <148719219+queeeety@users.noreply.github.com>
2024-09-09 14:51:22 +0100

    Initial Commit

---
+++ PhotoEncryptor/PhotoEncryptorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEncryptorApp.swift
+//  PhotoEncryptor
+//
+//  Created by Тимофій Безверхий on 09.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEncryptorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoEncryptorTests/PhotoEncryptorTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEncryptorTests.swift
+//  PhotoEncryptorTests
+//
+//  Created by Тимофій Безверхий on 09.09.2024.
+//
+
+import Testing
+@testable import PhotoEncryptor
+
+struct PhotoEncryptorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
